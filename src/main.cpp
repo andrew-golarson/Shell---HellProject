71efe2fc7aff5ac19c8e22fa3e849cfc1cffc15b
andrew-golarson <andrzejlesz14@gmail.com>
2025-12-06 22:33:24 +0100

    path without checking permission

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <filesystem>
+#include <cstdlib>
+#include <sstream>
+#include <algorithm>
+
+#ifdef _WIN32
+constexpr char PATH_SEPARATOR = ';';
+#else
+constexpr char PATH_SEPARATOR = ':';
+#endif
 
 int main() {
   std::vector<std::string> builtin{"echo", "type", "exit"};
@@ -8,19 +18,44 @@ int main() {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
     std::cout << "$ ";
+
     std::string command{};
     std::getline(std::cin, command);
     if(command.substr(0, 4) == "type"){
-      std::string typedCommand = command.substr(5, command.length() - 5);
-      if(find(builtin.begin(), builtin.end(), typedCommand) != builtin.end()){
-        std::cout << typedCommand << " is a shell builtin" << '\n';
+      std::string typed_command = command.substr(5, command.length() - 5);
+      if(std::find(builtin.begin(), builtin.end(), typed_command) != builtin.end()){
+        std::cout << typed_command << " is a shell builtin" << '\n';
+        continue;
       }else{
-        std::cerr << typedCommand << ": not found" << '\n';
+        const char* path_env = std::getenv("PATH");
+        std::string path_str(path_env);
+        std::string temp_split;
+        std::vector<std::filesystem::path> path_dirs;
+        std::stringstream ss(path_str);
+        while (std::getline(ss, temp_split, PATH_SEPARATOR)){
+          path_dirs.push_back(temp_split);
+        }
+        for(int i = 0; i<path_dirs.size(); i++){
+          std::filesystem::path curr_dir = path_dirs[i];
+          try{ 
+            for (const auto& dirEntry : std::filesystem::directory_iterator(curr_dir)){
+                std::filesystem::path curr_file = dirEntry.path();
+                if(curr_file.filename() == typed_command){
+                  std::cout << typed_command << " is " << curr_file << '\n';
+                  continue;
+                }
+            }
+          }catch(std::filesystem::__cxx11::filesystem_error err){}
+        }
       }
+      std::cerr << typed_command << ": not found" << '\n';
+       
     }else if(command.substr(0, 4) == "echo"){
       std::cout << command.substr(5, command.length() - 5) << '\n'; 
+
     }else if(command == "exit"){
         return 0;
+
     }else{
       std::cerr << command << ": command not found" << '\n';
     }

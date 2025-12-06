0a50e87858661d31258506b6851beae4d57504e7
andrew-golarson <andrzejlesz14@gmail.com>
2025-12-06 11:59:07 +0100

    stage 6

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  std::vector<std::string> builtin{"echo", "type", "exit"};
   while(true){
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
     std::cout << "$ ";
     std::string command{};
     std::getline(std::cin, command);
-    if(command.substr(0, 4) == "echo"){
-      std::cout << command.substr(5, command.length() - 4) << '\n'; 
+    if(command.substr(0, 4) == "type"){
+      if(find(builtin.begin(), builtin.end(), command.substr(5, command.length() - 5)) != builtin.end()){
+        std::cout << command.substr(5, command.length() - 5) << " is a shell builtin" << '\n';
+      }
+    }else if(command.substr(0, 4) == "echo"){
+      std::cout << command.substr(5, command.length() - 5) << '\n'; 
     }else if(command == "exit"){
         return 0;
     }else{
